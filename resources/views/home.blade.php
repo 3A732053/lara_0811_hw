4dd40464a533fd846bb0270ac2b8276d97df4773
Lin Yu Lun <wuyahayaa01@gmail.com>
2020-10-27 17:36:31 +0800

    Controller index內容新增

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,6 +24,8 @@
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    Route::get('/',[\App\Http\Controllers\AboutController::class, 'index'])->name('about.index');
+                    Route::get('/',[\App\Http\Controllers\NewsController::class, 'index'])->name('news.index');
                     @auth
                         <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
                     @else
